174218574fe1185a759da44531f9d1c1861ca4a9
NoorFatima1818 <159902924+NoorFatima1818@users.noreply.github.com>
2024-11-10 12:06:10 -0800

    Fixed unused variables and unescaped characters

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import { section } from 'framer-motion/client'
 import React from 'react'
 import SectionHeading from './section-heading'
